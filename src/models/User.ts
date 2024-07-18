1b7a2febd6ee718ee3749b84a4d7cd4c102361e5
jydnbach <jaydenkim95@gmail.com>
2024-07-18 21:41:46 +0900

    type alias for on method

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,6 +3,9 @@ interface UserProps {
   age?: number;
 }
 
+//type alias
+type Callback = () => {};
+
 export class User {
   constructor(private data: UserProps) {}
 
@@ -13,4 +16,6 @@ export class User {
   set(update: UserProps): void {
     Object.assign(this.data, update);
   }
+
+  on(eventName: string, callback: Callback) {}
 }
