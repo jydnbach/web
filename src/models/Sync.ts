570671cdad00f9f073659bbc64ccb517682e8175
jydnbach <jaydenkim95@gmail.com>
2024-07-21 14:30:08 +0900

    extraction to Sync.ts

---
+++ src/models/Sync.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosResponse } from 'axios';
+import { User } from './User';
+
+export class Sync extends User {
+  fetch(): void {
+    axios
+      .get(`http://localhost:3000/users/${this.get('id')}`)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data);
+      });
+  }
+
+  save(): void {
+    const id = this.get('id');
+
+    if (id) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      axios.post('http://localhost:3000/users', this.data);
+    }
+  }
+}
