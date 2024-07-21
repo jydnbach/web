ffe40c028b2c5d6fb959e3513ff292e7a2c2311a
jydnbach <jaydenkim95@gmail.com>
2024-07-21 16:22:41 +0900

    advanced generic constraint object keyof type

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,11 +1,28 @@
+import { UserProps } from './User';
+
 export class Attributes<T extends object> {
   constructor(private data: T) {}
 
-  get(propName: string): number | string {
-    return this.data[propName];
+  // K: some type … keyoftype
+  // K can only be 1 of those 3 type T (…userprops)
+  // T[K] object lookup
+
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
   }
 
   set(update: T): void {
     Object.assign(this.data, update);
   }
 }
+
+// *keys of an object can be a type as well*
+
+const attrs = new Attributes<UserProps>({
+  id: 5,
+  age: 20,
+  name: 'fdsaf',
+});
+const name = attrs.get('name');
+const age = attrs.get('age');
+const id = attrs.get('id');
