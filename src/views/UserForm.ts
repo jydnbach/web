0fc3b84b85db64da39c4f85a45c9589375082fc2
jydnbach <jaydenkim95@gmail.com>
2024-07-22 16:56:02 +0900

    binding event handlers

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -4,7 +4,12 @@ export class UserForm {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:button': this.onButtonClick,
+      'mouseover:h1': this.onHeaderHover,
     };
+  }
+
+  onHeaderHover(): void {
+    console.log('hover');
   }
 
   onButtonClick(): void {
@@ -20,9 +25,23 @@ export class UserForm {
     </div>`;
   }
 
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  }
+
   render(): void {
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
+
+    this.bindEvents(templateElement.content);
 
     this.parent.append(templateElement.content);
   }
