f39d7e1c60e03506afb8beed23865320e6e2307e
jydnbach <jaydenkim95@gmail.com>
2024-07-22 09:59:21 +0900

    get accessor on fullname

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@ import { User } from './models/User';
 
 const user = new User({ name: 'new record', age: 0 });
 
-user.save();
+// quick reminder on accessors
 
-user.events.on('change', () => {
-  console.log('change');
-});
+class Person {
+  constructor(public firstName: string, public lastName: string) {}
 
-user.events.trigger('change');
+  get fullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+const person = new Person('firstname', 'lastname');
+
+console.log(person.fullName);
