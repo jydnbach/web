4d95e51eeecb282356579d3db1e749d767c4bdb1
jydnbach <jaydenkim95@gmail.com>
2024-07-22 16:06:29 +0900

    render method on user form

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,11 +1,18 @@
 export class UserForm {
-  parent: Element;
+  constructor(public parent: Element) {}
 
   template(): string {
     return `
     <div>
-    <h1>User Form</h1>
-    <input/>
+      <h1>User Form</h1>
+      <input/>
     </div>`;
+  }
+
+  render(): void {
+    const templateElement = document.createElement('template');
+    templateElement.innerHTML = this.template();
+
+    this.parent.append(templateElement.content);
   }
 }
