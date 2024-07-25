5c6c6c0b2d8a09f30c4bb42185f5ff23167d1d8d
jydnbach <jaydenkim95@gmail.com>
2024-07-25 20:42:55 +0900

    reading input text button click setNameClick

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -16,8 +16,17 @@ export class UserForm {
       // 'click:button': this.onButtonClick,
       // 'mouseover:h1': this.onHeaderHover,
       'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick,
     };
   }
+
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector('input');
+
+    const name = input.value;
+
+    this.model.set({ name });
+  };
 
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
@@ -30,7 +39,7 @@ export class UserForm {
       <div>User name: ${this.model.get('name')}</div>
       <div>User age: ${this.model.get('age')}</div>
       <input/>
-      <button>Click me</button>
+      <button class="set-name">Change name</button>
       <button class="set-age">Set random age</button>
     </div>`;
   }
