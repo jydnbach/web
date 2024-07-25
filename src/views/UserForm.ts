8d91b7b0fa1fbc3298be9aa68f928777abef8f60
jydnbach <jaydenkim95@gmail.com>
2024-07-25 22:53:24 +0900

    button save: saving data from a view

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -8,8 +8,13 @@ export class UserForm extends View<User, UserProps> {
       // 'mouseover:h1': this.onHeaderHover,
       'click:.set-age': this.onSetAgeClick,
       'click:.set-name': this.onSetNameClick,
+      'click:.save-model': this.onSaveClick,
     };
   }
+
+  onSaveClick = (): void => {
+    this.model.save();
+  };
 
   onSetNameClick = (): void => {
     const input = this.parent.querySelector('input');
@@ -29,12 +34,10 @@ export class UserForm extends View<User, UserProps> {
   template(): string {
     return `
     <div>
-      <h1>User Form</h1>
-      <div>User name: ${this.model.get('name')}</div>
-      <div>User age: ${this.model.get('age')}</div>
-      <input/>
+      <input placeholder="${this.model.get('name')}"/>
       <button class="set-name">Change name</button>
       <button class="set-age">Set random age</button>
+      <button class="save-model">Save user</button>
     </div>`;
   }
 }
